6519da69fbd0f35971dc32caf37e3d375ca1ccaa
ghimireprakaash <ghimireprakash977@gmail.com>
2023-10-18 12:42:33 +0545

    nav responsive update

--- frontend/src/components/layouts/header/HeaderComponents.tsx
+++ frontend/src/components/layouts/header/HeaderComponents.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { LocalMall, PersonRounded, ShoppingCart } from "@mui/icons-material";
 import SearchBarComponents from "./SearchBarComponents";
+import NavBar from "./nav/NavBar";
 // import Login from "../pages/auth/Login";
 // import { Button } from "reactstrap";
 // import PopUpLoginComponent from "../pages/auth/PopUpLoginComponent";
@@ -13,64 +14,43 @@ function HeaderComponents() {
     <React.Fragment>
       <div className="top-header">
         <div className="first-header">
-          <Link to="/" className="logo">
-            <LocalMall sx={{ fontSize: 40, color: "white" }} />
-            <h1>Store</h1>
-          </Link>
+          <div className="header-logo-container">
+            <Link to="/" className="header-logo">
+              <LocalMall sx={{ fontSize: 40, color: "white" }} />
+              <h1>Store</h1>
+            </Link>
+          </div>
 
           <SearchBarComponents />
 
-          <ul>
-            <li>
-              {/* <Button
+          <div className="top-nav-links">
+            <ul>
+              <li>
+                {/* <Button
                 style={{ background: "transparent", border: "none" }}
                 onClick={PopUpLoginComponent}
               >
                 <PersonRounded sx={{ fontSize: 30, color: "white" }} />
               </Button> */}
-              {/* <Link to="" onClick={() => Login}>
+                {/* <Link to="" onClick={() => Login}>
                 <PersonRounded sx={{ fontSize: 30, color: "white" }} />
               </Link> */}
-              <Link to="/login">
-                <PersonRounded sx={{ fontSize: 30, color: "white" }} />
-              </Link>
-            </li>
-            <li>
-              <Link to="">
-                <ShoppingCart sx={{ fontSize: 30, color: "white" }} />
-              </Link>
-            </li>
-          </ul>
+                <Link to="/login">
+                  <PersonRounded sx={{ fontSize: 30, color: "white" }} />
+                </Link>
+              </li>
+              <li>
+                <Link to="">
+                  <ShoppingCart sx={{ fontSize: 30, color: "white" }} />
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="second-header">
-          <ul>
-            <li>
-              <Link to="/" className="nav-items">
-                Categories
-              </Link>
-            </li>
-            <li>
-              <Link to="/" className="nav-items">
-                Mobile Phones
-              </Link>
-            </li>
-            <li>
-              <Link to="/" className="nav-items">
-                Mobile Accessories
-              </Link>
-            </li>
-            <li>
-              <Link to="/" className="nav-items">
-                Laptops
-              </Link>
-            </li>
-            <li>
-              <Link to="/" className="nav-items">
-                PC Components
-              </Link>
-            </li>
-          </ul>
-        </div>
+
+        {/* NavBar component */}
+        <NavBar />
+        {/* NavBar End */}
       </div>
     </React.Fragment>
   );
--- frontend/src/components/routes/RouterComponents.tsx
+++ frontend/src/components/routes/RouterComponents.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Router } from "react-router-dom";
 import HomeComponents from "../pages/home/HomeComponents";
 import LoginComponents from "../pages/auth/LoginComponents";
 import PageNotFound from "../pages/errors/PageNotFound";
@@ -11,16 +11,23 @@ function RouterComponents() {
   return (
     <React.Fragment>
       <Routes>
+        {/* Incase the path doesn't exist */}
+        <Route path="*" element={<PageNotFound />} />
+        {/* Incase the path doesn't exist */}
+
         <Route path="/" element={<HomeComponents />} />
         <Route path="/login" element={<LoginComponents />} />
         <Route path="/register" element={<RegisterComponent />} />
-        <Route path="/dashboard" element={<DashboardComponent />} />
-        <Route
-          path="/dashboard/manage-category"
-          element={<CategoryComponent />}
-        />
-
-        <Route path="*" element={<PageNotFound />} />
+        <Route path="/dashboard">
+          <Route path="/dashboard" element={<DashboardComponent />} />
+          <Route
+            path="/dashboard/manage-category"
+            element={<CategoryComponent />}
+          />
+          {/* <Route path="/dashboard/*">
+            <Route path="/dashboard" element={<DashboardComponent />} />
+          </Route> */}
+        </Route>
       </Routes>
     </React.Fragment>
   );
